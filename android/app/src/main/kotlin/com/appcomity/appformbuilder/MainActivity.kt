3da5aaa2f3420a1b846cf8cd51559a72522492cd
Stefan Ronnkvist <118016526+StefanRonnkvist@users.noreply.github.com>
2024-09-04 19:07:25 -0500

    Initial commit

---
+++ android/app/src/main/kotlin/com/appcomity/appformbuilder/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.appcomity.appformbuilder
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'codebackup.dart';
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'App Comity',
+      theme: ThemeData(
+        primarySwatch: Colors.lightGreen,
+      ),
+      home: const MyHomePage(title: 'Home page'),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: const SingleChildScrollView(
+        child: Center(
+          child: Padding(
+            padding: EdgeInsets.all(8.0),
+            child: SizedBox(
+              width: 500,
+              child: Column(
+                children: [
+                  Text(
+                    'Welcome to App Comity!',
+                  ),
+                  CompleteForm(),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
